2c6dacd94a0a84760eda863bd5a34e5d3fd39a5f
bruna875 <bruna@saykudos.co>
2026-04-08 16:54:11 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,22 +1,14 @@
-export const config = { runtime: "edge" };
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
 
-export default async function handler(req) {
   const SHEET_URL = "https://script.google.com/macros/s/AKfycbwFrVCmNoPCZ7OiDlM1bUaYrf7K9Na5Uq1-5SSfywzGYNgA3ut-pLlPzxUvaXzb3-CC/exec";
 
   try {
     const response = await fetch(SHEET_URL);
     const data = await response.json();
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
-    });
+    res.status(200).json(data);
   } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" }
-    });
+    res.status(500).json({ error: err.message });
   }
 }
