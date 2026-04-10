2bc6ce16db91d1181d0ff1f6c23396fa27c49140
bruna875 <bruna@saykudos.co>
2026-04-10 16:02:49 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
-  const SHEET_URL = "https://script.google.com/macros/s/AKfycbwFrVCmNoPCZ7OiDlM1bUaYrf7K9Na5Uq1-5SSfywzGYNgA3ut-pLlPzxUvaXzb3-CC/exec";
+  const SHEET_URL = "https://script.google.com/macros/s/AKfycbxV0z5qf0I0U4YiKivIm6S84s-CPyuevY4OAnBw7rftIoZi41KvHA5rGAihThM6GJxXNQ/exec";
 
   try {
     const response = await fetch(SHEET_URL);
